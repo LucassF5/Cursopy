54e347e8e2fdc2de0e0e3d91fd0ee52694e9e955
L F <franco100lucas@gmail.com>
2023-04-29 16:59:22 -0300

    extra aula 3

---
+++ aula3.1-id.py
@@ -0,0 +1,9 @@
+# id = Identidade
+# id é o local da memória onde está guardada a informação do objeto em questão
+
+v1 = 'info'
+v2 = 'info'
+
+print(id(v1))
+print(id(v2))
+# Como a informação dentro das variáveis é a mesma, então o local pode acabar sendo o mesmo
