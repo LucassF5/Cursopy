28b597fc7c759a1186b83134361a05985fdd0d96
L F <franco100lucas@gmail.com>
2023-05-19 06:00:08 -0300

    Salvando todo em JSON

--- aula67-Todo exercise(meu code).py
+++ aula67-Todo exercise(meu code).py
@@ -9,15 +9,12 @@
 # refazer = todo ['fazer café']
 # refazer = todo ['fazer café', 'caminhar']
 
+import json
 import os
 
 
 def limpa_tela():
     os.system("cls")
-
-
-todo = []
-desfazer = []
 
 
 def mostra_itens(lista):
@@ -50,9 +47,32 @@ def refazer(lista):
     return lista, desfazer
 
 
+def ler(tarefas, caminho_arquivo):
+    dados = []
+    try:
+        with open(caminho_arquivo, 'r', encoding='utf8') as arquivo:
+            dados = json.load(arquivo)
+    except FileNotFoundError:
+        print('Arquivo não existe')
+        salvar(tarefas, caminho_arquivo)
+    return dados
+
+
+def salvar(tarefas, caminho_arquivo):
+    dados = tarefas
+    with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+        dados = json.dump(tarefas, arquivo, indent=2, ensure_ascii=False)
+    return dados
+
+
+CAMINHO_ARQUIVO = 'aula67.json'
+todo = ler([], CAMINHO_ARQUIVO)
+desfazer = []
+
+
 def roda_programa():
     while True:
-        print('Comandos: listar, desfazer, refazer e clear')
+        print('Comandos: listar, desfazer, refazer, clear e sair')
         nome = input('Digite uma tarefa ou comando:\n>>> ')
         if nome == 'listar':
             print()
@@ -67,8 +87,9 @@ def roda_programa():
         elif nome == 'refazer':
             refazer(todo)
 
+        elif nome == 'sair':
+            break
+
         else:
             add_word(nome, todo)
-
-
-roda_programa()
+        salvar(tarefas, CAMINHO_ARQUIVO)
