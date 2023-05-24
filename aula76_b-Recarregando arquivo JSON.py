62ac617e5907c14eb932ee865b5a1bf6b626834e
L F <franco100lucas@gmail.com>
2023-05-24 11:10:26 -0300

    Recarregando arquivo Json para a classe

--- aula76_b-Recarregando arquivo JSON.py
+++ aula76_b-Recarregando arquivo JSON.py
@@ -4,6 +4,7 @@ from aula76_a import CAMINHO_ARQUIVO, Pessoa, fazer_dump
 
 with open(CAMINHO_ARQUIVO, 'r') as arquivo:
     pessoas = json.load(arquivo)
+    # Expandindo dicionário para poder selecionar e fazer o carregamento de arquivos
     p1 = Pessoa(**pessoas[0])
     p2 = Pessoa(**pessoas[1])
     p3 = Pessoa(**pessoas[2])
