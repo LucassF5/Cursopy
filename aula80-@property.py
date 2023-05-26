1c4f4f8b32b2fa09d69adacad091cda65e8b2d31
L F <franco100lucas@gmail.com>
2023-05-26 01:03:36 -0300

    @property - um getter no modo Pythônico

---
+++ aula80-@property.py
@@ -0,0 +1,58 @@
+# @property - um getter no modo Pythônico
+# getter - um método para obter um atributo
+# cor -> get_cor()
+# modo pythônico - modo do Python de fazer coisas
+# @property é uma propriedade do objeto, ela
+# é um método que se comporta como um
+# atributo 🤯 🤯 🤯
+# Geralmente é usada nas seguintes situações:
+# - como getter
+# - p/ evitar quebrar código cliente
+# - p/ habilitar setter
+# - p/ executar ações ao obter um atributo
+# Código cliente - é o código que usa seu código
+class Caneta:
+    def __init__(self, cor):
+        self.cor_tinta = cor
+
+    # método que se comporta como um atributo
+    @property
+    def cor(self):  # Aqui está sendo usada como método
+        print('PROPERTY')
+        return self.cor_tinta
+
+    @property
+    def cor_tampa(self):
+        return 123456
+
+###########################
+
+
+caneta = Caneta('Azul')
+print(caneta.cor)  # Aqui está sendo usada como atributo, pois não precisa de ()
+print(caneta.cor)
+print(caneta.cor)
+print(caneta.cor)
+print(caneta.cor)
+print(caneta.cor)
+print(caneta.cor_tampa)
+
+#####################################
+
+# class Caneta:
+#     def __init__(self, cor):
+#         self.cor_tinta = cor
+
+#     def get_cor(self):
+#         print('GET COR')
+#         return self.cor_tinta
+
+# ###########################
+
+
+# caneta = Caneta('Azul')
+# print(caneta.get_cor())
+# print(caneta.get_cor())
+# print(caneta.get_cor())
+# print(caneta.get_cor())
+# print(caneta.get_cor())
