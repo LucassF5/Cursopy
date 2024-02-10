5397e651ea82dea4051c5d2b3d0130a665ea2a75
L F <franco100lucas@gmail.com>
2024-02-10 09:39:47 -0300

    Adicionando fields

--- aula106-Dataclasses.py
+++ aula106-Dataclasses.py
@@ -1,17 +1,21 @@
 # dataclasses - O que são dataclasses?
 # O módulo dataclasses fornece um decorador e funções para criar métodos como
-# __init__(), __repr__(), __eq__() (entre outros) em classes definidas pelo
+# __init__(), __repr__(), __eq__() (entre outros) em classes definidas pelo 
 # usuário.
 # Em resumo: dataclasses são syntax sugar para criar classes normais.
 # Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
 # doc: https://docs.python.org/3/library/dataclasses.html
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass(init=False)
 class Pessoa:
-    nome: str
-    sobrenome: str
+    nome: str = field(
+        default='Lucas',
+    )
+    sobrenome: str = field(
+        default='Franco',
+    )
 
 # Declarando init=False, o método __init__ não é criado
     def __init__(self, nome, sobrenome):
@@ -35,6 +39,6 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    p1 = Pessoa('Luiz', 'Otávio')
+    p1 = Pessoa("Lucas", "Franco")
     print(p1)
     print(p1.nome_completo)
